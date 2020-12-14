d4bdb7fad0b13e0c24527cdfca5c6e7a9c81c4c7
niemx <1937352544@qq.com>
2020-12-14 17:08:58 +0800

    '推送轮播图'

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// 倒计时
 function timer() {
     var future = new Date('2021/1/1 00:00:00')
     var now = new Date
@@ -12,6 +13,23 @@ function timer() {
     datetime += seconds + ' 秒 '
     document.getElementById("timeOut").innerHTML = datetime;
 }
-
 timer();
 setInterval(timer, 1000);
+
+// 轮播图
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: true, //可选选项，自动滑动
+})
+
+//如果你初始化时没有定义Swiper实例，后面也可以通过Swiper的HTML元素来获取该实例
+new Swiper('.swiper-container')
+var mySwiper = document.querySelector('.swiper-container').swiper
+mySwiper.slideNext();
+var hotPrev = document.querySelector('.hot_prev');
+var hotNext = document.querySelector('.hot_next');
+hotNext.addEventListener('click', function() {
+    mySwiper.slideNext()
+})
+hotPrev.addEventListener('click', function() {
+    mySwiper.slidePrev()
+})
